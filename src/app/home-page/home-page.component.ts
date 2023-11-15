cd842dbd10f87c5342ff432e202e56262a46e992
nandika <dedungaz123@gmail.com>
2023-11-15 14:19:03 +0800

    add a view encapsulation

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HomePageComponent {
 
