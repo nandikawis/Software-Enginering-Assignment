9649c2bd1838915557fa93abf6f597264b8ae553
nandika <dedungaz123@gmail.com>
2023-11-20 01:22:13 +0800

    do a lot of modify in home-page and tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,12 @@ module.exports = {
       fontFamily: {
         sans: ['Fira Sans', 'sans-serif'],
       },
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1440px',
+      },
     },
   },
   plugins: [],
