112e844471c4ba3402e87c51f15681281d59b6c8
nandika <dedungaz123@gmail.com>
2023-11-21 00:32:42 +0800

    restructure it into SPA

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirect empty path to '/home'
-  { path: 'home', component: HomePageComponent }, // HomePage route
+  {
+    path: 'home', component: HomePageComponent, children: [
+      { path: '', redirectTo: 'landing-page', pathMatch: 'full' },
+      { path: 'landing-page', component: LandingPageComponent }
+    ]
+  }, // HomePage route
   // Add more routes here as needed
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { MerchantPageComponent } from './merchant-page/merchant-page.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    MerchantPageComponent
+    MerchantPageComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
