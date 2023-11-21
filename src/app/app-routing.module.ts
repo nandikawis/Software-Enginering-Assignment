332f928780ae8443904b0ed6d303470329cfb76b
Hasegawa Rin <hasegawarin@zhangguchuanlins-MacBook-Pro.local>
2023-11-21 17:19:12 +0800

    create merchant component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
+import { MerchantPageComponent } from './merchant-page/merchant-page.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirect empty path to '/home'
-  { path: 'home', component: HomePageComponent }, // HomePage route
+  { path: '', redirectTo: '/merchant', pathMatch: 'full' }, // Redirect empty path to '/home'
+  { path: 'merchant', component: MerchantPageComponent }, // HomePage route
   // Add more routes here as needed
 ];
 
